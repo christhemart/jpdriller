a43361c82c8e145217d154a0a264f83353cba781
Chris Martinez <christhemart@gmail.com>
2018-05-07 17:16:53 -0500

    Ajax Implementation
    Added Ajax call to grab new vocabulary fro questioning.

--- website/jpdriller/views.py
+++ website/jpdriller/views.py
@@ -4,9 +4,19 @@ from django.shortcuts import render
 
 from .models import *
 
+from django.http import JsonResponse
+from django.http import HttpResponse
+
 
 def index(request):
-    groups = Vocabulary.objects.values('group').distinct()
-    #vocabulary = vocabulary[random.randint(0,len(vocabulary)-1)]
+    groups = list(Vocabulary.objects.values('group').distinct())
     context = {'groups':groups}
     return render(request, 'jpdriller/index.html',context)
+    
+def get_vocabulary(request):
+    groups = request.GET['groups'].split(',') 
+    vocab = Vocabulary.objects.filter(group__in=groups)
+
+    vocab = vocab[random.randint(0,len(vocab)-1)]
+    response = str(vocab.vocabulary)+","+str(vocab.pronounciation)+","+str(vocab.translation)
+    return HttpResponse(response)
