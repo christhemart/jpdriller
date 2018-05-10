a2c07e93e5d36618eebaf6d457000c2b88bf7099
Chris Martinez <christhemart@gmail.com>
2018-05-10 17:11:04 -0500

    English to Japanese.
    Added into randomization to ask for Japanese translations from English questions. Finished minimal styleing.

--- website/jpdriller/views.py
+++ website/jpdriller/views.py
@@ -9,11 +9,17 @@ def index(request):
     groups = list(Vocabulary.objects.values('group').distinct())
     context = {'groups':groups}
     return render(request, 'jpdriller/index.html',context)
-    
+
 def get_vocabulary(request):
-    groups = request.GET['groups'].split(',') 
+    groups = request.GET['groups'].split(',')
     vocab = Vocabulary.objects.filter(group__in=groups)
 
-    vocab = vocab[random.randint(0,len(vocab)-1)]
-    response = str(vocab.vocabulary)+','+str(vocab.pronounciation)+','+str(vocab.translation)+','+str(vocab.note)+','+str(vocab.group)
+    vocab = vocab[random.randint(0, len(vocab) - 1)]
+
+    if str(vocab.group) not in ['Hiragana', 'Katakana']:
+        response = str(random.randint(0, 1))
+    else:
+        response = '0'
+
+    response += ','+str(vocab.vocabulary)+','+str(vocab.pronounciation)+','+str(vocab.translation)+','+str(vocab.note)+','+str(vocab.group)
     return HttpResponse(response)
